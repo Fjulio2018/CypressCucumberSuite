1e385f503ae5b2b410ca0c631d09f5f501b4cbdb
Francisnasc <juliomariano2005@yahoo.com.br>
2024-12-20 16:46:09 -0300

    Cucumber config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+      return config;
     },
+    specPattern: "cypress/e2e/**/*.feature", // Define onde encontrar os arquivos .feature
+    baseUrl: "http://localhost:8080", // Substitua pelo URL base do seu site de testes, se necessário
   },
 });
